ab319ca11e137a8829fbe1c99c3b3bdd4a82f777
Victor Trifan <75999988+victort9@users.noreply.github.com>
2021-11-10 11:08:25 +0100

    Update Trip_duration_stats.sql

--- Query/Trip_duration_stats.sql
+++ Query/Trip_duration_stats.sql
@@ -1,12 +1,12 @@
 --Creating new column "trip_duration"
-ALTER TABLE clean
+ALTER TABLE Cyclists
 ADD trip_duration TIME
-UPDATE clean
+UPDATE Cyclists
 SET trip_duration = (julianday(ended_at)-julianday(started_at))*1440
 
 
 --deleting all the outliers (40974rows)
-DELETE FROM clean
+DELETE FROM Cyclists
 WHERE trip_duration <=0.45 AND start_station_id = end_station_id
 OR trip_duration <=0
 
@@ -17,6 +17,6 @@ avg(trip_duration) AS Mean,
 max(trip_duration) AS Max,
 min(trip_duration) AS Min,
 count(trip_duration) AS TPD
-FROM clean
+FROM Cyclists
 GROUP BY ymd, member_casual
 
